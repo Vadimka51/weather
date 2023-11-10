38063de9cc7bdf90fd85d951cc3bcf8d1995ef7d
Vadimka51 <ocirovvadim51@gmail.com>
2023-11-10 18:46:55 +1200

    params

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import requests
 
 
 user_city = input('Введите город: ')
-url = f"https://wttr.in/{user_city}"
+url = f"https://wttr.in/{user_city}?ru"
 
+params = {
+    "n" : '',
+    "m" : '',
+    "ru": '',
+    "1" : ''
+}
 
-response = requests.get(url)
+response = requests.get(url, params = params)
 response.raise_for_status()
 print(response.text)
